4f21ee6f3a5fcf25b92656e4d7ccc1883c48d3f1
Claudio Guerra <claudevandort@gmail.com>
2022-05-25 15:13:27 +0800

    feat(player): implement jump & idle player behavior

--- Super Platzi Boy/Assets/Scripts/PlayerController.cs
+++ Super Platzi Boy/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float jumpForce = 6f;
+    Rigidbody2D rigidBody;
+    Animator animator;
+
+    const string STATE_MOVING = "isMoving";
+    const string STATE_ON_THE_GROUND = "isOnTheGround";
+
+    public LayerMask groundMask;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator.SetBool(STATE_MOVING, false);
+        animator.SetBool(STATE_ON_THE_GROUND, false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        animator.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.2f, Color.red);
     }
+
+    void Jump()
+    {
+        if (IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool IsTouchingTheGround() => Physics2D.Raycast(this.transform.position, Vector2.down, 1.2f, groundMask);
 }
